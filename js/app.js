6aed248b04f3f73922f1e28b56bf78726243a410
SirAbbott <tom@iynk.com>
2019-01-06 15:28:07 +0000

    logic for moving a horizontal piece around the board

--- js/app.js
+++ js/app.js
@@ -14,6 +14,22 @@
 // Select one
 // Store the correct number in it
 
+// PSEUDOCODE FOR PLAYER MOVE
+// Create a player 1 with an empty array (gridWidth*gridWidth)
+// Add eventListener for each piece
+// When you click on each ship it appears (top left)
+// Use keyboard to move piece up down left right
+// Make sure that the piece can't move over the edges
+// Space to rotate the pieces
+// Enter to save
+// When you save, store the correct values into the player1
+// pieceValue = 4
+// piecePosition = [0,1,2,3]
+// Press right
+// currentPiece = [1,2,3,4]
+// loop through and save to the index values (above) the correct value for the ship to player1Original
+// once all pieces are placed, start game, cpu player should generate own board
+
 //https://github.com/LearnTeachCode/Battleship-JavaScript/blob/gh-pages/battleship.js
 
 // var u = undefined
@@ -39,33 +55,40 @@ $(() => {
   let player1
   let cpuPlayerOriginal
   let cpuPlayer
+  let hitcount = 0
+  // const $startgameButton = $('start-game')
   const $ships = $('.ship')
   const ships = [{
-    name: 'Patrol Boat',
-    color: 'yellow',
-    l: 2,
-    v: 2
-  }, {
-    name: 'Destroyer',
-    color: 'blue',
-    l: 3,
-    v: 3
-  }, {
-    name: 'Submarine',
-    color: 'orange',
-    l: 3,
-    v: 4
-  }, {
-    name: 'Battleship',
-    color: 'green',
-    l: 4,
-    v: 5
-  }, {
-    name: 'Aircraft Carrier',
-    color: 'purple',
-    l: 5,
-    v: 6
-  }]
+      name: 'Patrol Boat',
+      color: 'Patrol Boat',
+      l: 2,
+      v: 2
+    },
+    {
+      name: 'Destroyer',
+      color: 'blue',
+      l: 3,
+      v: 3
+    },
+    {
+      name: 'Submarine',
+      color: 'orange',
+      l: 3,
+      v: 4
+    },
+    {
+      name: 'Battleship',
+      color: 'green',
+      l: 4,
+      v: 5
+    },
+    {
+      name: 'Aircraft Carrier',
+      color: 'purple',
+      l: 5,
+      v: 6
+    }
+  ]
 
   function createBoards() {
     for (let i = 0; i < gridWidth * gridWidth; i++) {
@@ -74,13 +97,10 @@ $(() => {
       $board2.append($('<div>'))
     }
   }
-
   createBoards()
-
 
   const $squares = $board.find('div')
   const squaresArray = Array.from($squares)
-
   $squares.on('click', e => {
     const index = squaresArray.indexOf(e.target)
     checkValue(index, e)
@@ -92,14 +112,11 @@ $(() => {
     cpuPlayerOriginal = Array.apply(undefined, {
       length: gridWidth * gridWidth
     })
-
     // Loop through all ship pieces and find a space for it...
     ships.forEach(ship => checkForValidMove(cpuPlayerOriginal, ship))
-
     // make a copy of the array for the game (i.e. sunk and miss shots etc)
     cpuPlayer = [...cpuPlayerOriginal]
   }
-
   createCpuPlayer()
 
   function createPlayer1() {
@@ -120,22 +137,67 @@ $(() => {
     console.log(player1[index])
   })
 
-
-  $ships.on('click', (e) => {
+  $ships.on('click', e => {
+    // let counter = 0;
     const clickedShip = $(e.target)
     // take data attribute from HTML and assign to the event target
     const selectedShipName = clickedShip.data('name')
     // console.log(selectedShipName)
     // make the event-target our object
     const selectedShip = ships.find(ships => ships.name === selectedShipName)
-    // console.log(selectedShip)
+    console.log(selectedShip)
     // replace the indexes of array by the ship l
-    // player1Original = selectedShip.v
+    const shipPosition = []
     for (let i = 0; i < selectedShip.l; i++) {
+      shipPosition.push(i)
+      player1Original[i] = selectedShip.v
       $(squaresArray2[i]).css({
         'background-color': selectedShip.color
       })
     }
+    //event listener for key strokes
+    $(document).on('keydown', e => {
+      // loop through shipPosition then move all.
+      // left 37, up 38, right 39, down 40
+      switch (e.keyCode) {
+        case 37:
+          // So this will just loop through... You need to do this:
+          shipPosition.forEach(
+            (position, i) => (shipPosition[i] = position - 1)
+          )
+          console.log('left', shipPosition)
+
+          break
+        case 38:
+          // if (shipPosition - gridWidth >= 0) {
+          shipPosition.forEach(
+            (position, i) => (shipPosition[i] = position - gridWidth)
+          )
+          console.log('up', shipPosition)
+
+          break
+        case 39:
+          shipPosition.forEach(
+            (position, i) => (shipPosition[i] = position + 1)
+          )
+          console.log('right', shipPosition)
+
+          break
+        case 40:
+          // if (shipPosition + gridWidth < gridWidth * gridWidth) {
+          shipPosition.forEach(
+            (position, i) => (shipPosition[i] = position + gridWidth)
+          )
+          console.log('down', shipPosition)
+          // }
+          break
+      }
+    })
+    // counter++;
+    // console.log(counter);
+    // if (counter === 5) {
+    //   $startgameButton.addClass();
+    // }
   })
 
   function checkValue(index, e) {
@@ -152,6 +214,8 @@ $(() => {
       $(e.target).css({
         'background-color': 'red'
       })
+      hitcount++
+      console.log('hitcount', hitcount)
       // Check to see if the array still contains any of the same value
       // If not, then the ship must have been sunk!
       if (cpuPlayer.includes(value)) {
@@ -166,9 +230,11 @@ $(() => {
         // The length would give you which ship has been sunk...
         console.log(sunkIndexes.length)
         // Turn them all black
-        sunkIndexes.forEach(sunk => $(squaresArray[sunk]).css({
-          'background-color': 'black'
-        }))
+        sunkIndexes.forEach(sunk =>
+          $(squaresArray[sunk]).css({
+            'background-color': 'black'
+          })
+        )
       }
       // You've missed
     } else {
@@ -208,7 +274,9 @@ $(() => {
         // If it is an undefined, add the index into the last array
         if (current === undefined) {
           // Push the ORIGINAL index value using the array of indexes
-          undefinedSquares[undefinedSquares.length - 1].push(arrayOfIndexes[index])
+          undefinedSquares[undefinedSquares.length - 1].push(
+            arrayOfIndexes[index]
+          )
         } else {
           // If it is not undefined, create a new array
           undefinedSquares.push([])
@@ -218,19 +286,23 @@ $(() => {
 
       // Filter the chunks by the size of the piece that you are placing
       // Red & Green
-      return undefinedSquares.filter(consecutiveUndefinedSquare => consecutiveUndefinedSquare.length >= ship.l)
+      return undefinedSquares.filter(
+        consecutiveUndefinedSquare =>
+        consecutiveUndefinedSquare.length >= ship.l
+      )
     })
 
     // Remove the outer array
     availableSpaces = availableSpaces.flat()
 
     // Remove all empty arrays where no move was possible in a row
-    // We only want the available spaces where the ship can actually fit
+    // I only want the available spaces where the ship can actually fit
     availableSpaces = availableSpaces.filter(available => available.length > 0)
 
     // Select random free chunk
     // This selected space is going to be ALL possible spaces, i.e [0,1,2,3,4]
-    const selectedSpace = availableSpaces[Math.floor(Math.random() * availableSpaces.length)]
+    const selectedSpace =
+      availableSpaces[Math.floor(Math.random() * availableSpaces.length)]
 
     // Build all possible moves in selected selectedSpace
     const possible = []
@@ -262,7 +334,6 @@ $(() => {
       })
     })
   }
-
 })
 
 // ----------------------------------------------
@@ -278,11 +349,11 @@ function chunkArray(board, gridWidth) {
 
 function rotateBoard(board, gridWidth) {
   const a = chunkArray(board, gridWidth)
-  const unflattened = a[0].map((col, c) => a.map((row, r) => a[r][c]).reverse())
+  const unflattened = a[0].map((col, c) =>
+    a.map((row, r) => a[r][c]).reverse()
+  )
   return unflattened.flat()
 }
-
-
 
 // function displayBoardInConsole(board, gridWidth) {
 //   // Split the board into rows
@@ -301,19 +372,3 @@ function rotateBoard(board, gridWidth) {
 //     console.log(tempRow)
 //   })
 // }
-
-// Create a player 1 with an empty array (gridWidth*gridWidth)
-// Add eventListener for each piece
-// When you click on each ship it appears (top left)
-// Use keyboard to move piece up down left right
-// Make sure that the piece can't move over the edges
-// Space to rotate the pieces
-// Enter to save
-// When you save, store the correct values into the player1
-
-// pieceValue = 4
-// piecePosition = [0,1,2,3]
-// Press right
-// currentPiece = [1,2,3,4]
-// loop through and save to the index values (above) the correct value for the ship to player1Original
-// once all pieces are placed, start game, cpu player should generate own board
